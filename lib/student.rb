4fe9a9732cae7670540d314b489039709fb5de80
Nick Inman <nick@theoatmeal.com>
2018-12-16 22:56:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
 
   def self.create_from_collection(students_array)
 
-    students_array.each do {|test| binding.pry }
+    students_array.each do {|x,y| binding.pry }
   end
 
   def add_student_attributes(attributes_hash)
