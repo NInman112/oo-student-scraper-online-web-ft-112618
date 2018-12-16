3600ba362d9b85899225be8e3eefe9efcab56f3d
Nick Inman <nick@theoatmeal.com>
2018-12-16 22:54:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,8 @@ class Student
 
   def initialize(student_hash)
     #binding.pry
-    name = student_hash[:name]
-    location = student_hash[:location]
+    @name = student_hash[:name]
+    @location = student_hash[:location]
   end
 
   def self.create_from_collection(students_array)
