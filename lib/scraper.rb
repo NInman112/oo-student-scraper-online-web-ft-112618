85a109366d78a0b4faa7284d30ccb9a5c09e188e
Nick Inman <nick@theoatmeal.com>
2018-12-16 20:25:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,13 +20,14 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     profile_hash = {}
+    i = 0
     doc = Nokogiri::HTML(open(profile_url))
     doc.css(".main-wrapper.profile").each do |profile_node|
         #binding.pry
-        profile_hash[:twitter] = profile_node.css(".vitals-container").css(".social-icon-container a")[0].nil? ? nil : profile_node.css(".vitals-container").css(".social-icon-container a")[0].attr("href")
-        profile_hash[:linkedin] = profile_node.css(".vitals-container").css(".social-icon-container a")[1].nil? ? nil : profile_node.css(".vitals-container").css(".social-icon-container a")[1].attr("href")
-        profile_hash[:github] = profile_node.css(".vitals-container").css(".social-icon-container a")[2].nil? ? nil : profile_node.css(".vitals-container").css(".social-icon-container a")[2].attr("href")
-        profile_hash[:blog] = profile_node.css(".vitals-container").css(".social-icon-container a")[3].nil? ? nil : profile_node.css(".vitals-container").css(".social-icon-container a")[3].attr("href")
+        profile_hash[:twitter] = profile_node.css(".vitals-container").css(".social-icon-container a")[i].nil? ? nil : profile_node.css(".vitals-container").css(".social-icon-container a")[0].attr("href")
+        profile_hash[:linkedin] = profile_node.css(".vitals-container").css(".social-icon-container a")[i].nil? ? nil : profile_node.css(".vitals-container").css(".social-icon-container a")[1].attr("href")
+        profile_hash[:github] = profile_node.css(".vitals-container").css(".social-icon-container a")[i].nil? ? nil : profile_node.css(".vitals-container").css(".social-icon-container a")[2].attr("href")
+        profile_hash[:blog] = profile_node.css(".vitals-container").css(".social-icon-container a")[i].nil? ? nil : profile_node.css(".vitals-container").css(".social-icon-container a")[3].attr("href")
 
         profile_hash[:profile_quote] = profile_node.css(".vitals-container").css(".vitals-text-container").children[5].text.strip
         profile_hash[:bio] = profile_node.css(".details-container").css(".bio-block.details-block").css(".bio-content.content-holder").css(".description-holder").text.strip
